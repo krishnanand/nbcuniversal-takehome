c93a8dc46ed2f9603b71bb4121be603b758add0d
manoflogan <manoflogan@gmail.com>
2017-12-05 05:01:42 -0800

    Json ignore quiz id

--- src/main/java/com/krishnanand/nbcuniversal/QuizQuestion.java
+++ src/main/java/com/krishnanand/nbcuniversal/QuizQuestion.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(Include.NON_NULL)
 public class QuizQuestion {
   
+  @JsonIgnore
   private String quizId;
   
   private int questionId;
